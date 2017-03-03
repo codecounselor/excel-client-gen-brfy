72afb7b0705f93d9b2524fa338f6d61b67380867
Nate Good <nate.good@spiderstrategies.com>
2017-03-03 10:00:17 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var saveAs = require('file-saver').saveAs
+var Workbook = require('./workbook')
+
+const ws_name = 'SheetJS'
+const data = [[1, 2, 3],
+  [true, false, null, 'sheetjs'],
+  ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']]
+
+const wb = new Workbook()
+const ws = wb.createSheet(data)
+wb.SheetNames.push(ws_name)
+wb.Sheets [ws_name] = ws
+
+//Do we need npm install --save blob.js?
+saveAs(new Blob(wb.getAsArrayBuffer(),{type:"application/octet-stream"}), "test.xlsx")
